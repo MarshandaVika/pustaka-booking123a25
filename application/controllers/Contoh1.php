218c6107a80937a8b2682f8423a4d6ce6145cd36
MarshandaVika <marshandavika05@gmail.com>
2023-09-26 20:47:13 +0700

    Membuat project CI 3 Pustaka Booking

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,12 @@
+<?php 
+class Contoh1 extends CI_Controller
+{
+    public function index(){
+    echo "<h1> Perkenalan </h1>";
+    echo "Nama Saya Siti
+    Saya tinggal di kolong jembatan
+    Olahraga yang saya sukai adalah nyopet";
+    }
+}
+
+?>
